9cdfafc66f7c49fe28db0671b7c0f175422a159c
Nate Brunette <nbrunett@nerdery.com>
2015-04-02 22:08:31 -0500

    Updating to use psr4 autoloading
    Signed-off-by: Nate Brunette <nbrunett@nerdery.com>

--- TebruRetrofitBundle.php
+++ TebruRetrofitBundle.php
@@ -7,7 +7,7 @@ namespace Tebru\RetrofitBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tebru\Retrofit\Retrofit;
+use Tebru\Retrofit\Provider\GeneratedClassMetaDataProvider;
 use Tebru\RetrofitBundle\DependencyInjection\Compiler\RegisterCompilerPass;
 
 /**
@@ -17,29 +17,11 @@ use Tebru\RetrofitBundle\DependencyInjection\Compiler\RegisterCompilerPass;
  */
 class TebruRetrofitBundle extends Bundle
 {
-    /**
-     * Track if retrofit has already been loaded
-     *
-     * Running cache:clear will call this boot method twice
-     * and throw errors if we try to require the file twice
-     *
-     * @var bool
-     */
-    private static $retrofitLoaded = false;
-
-    /**
-     * Load cache file
-     */
     public function boot()
     {
-        if (true === self::$retrofitLoaded) {
-            return null;
-        }
-
         $cacheDir = $this->container->getParameter('kernel.cache_dir');
-        $retrofit = new Retrofit($cacheDir);
-        $retrofit->load();
-        self::$retrofitLoaded = true;
+        $loader = require __DIR__ . '/../../autoload.php';
+        $loader->addPsr4(GeneratedClassMetaDataProvider::NAMESPACE_PREFIX . '\\', $cacheDir . '/retrofit');
     }
 
     /**
